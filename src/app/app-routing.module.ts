3f78f6fc0e3697debe66ec2efb7f0417862d0719
RanitaDeMay <fecamarenafl@ittepic.edu.mx>
2023-11-06 17:49:26 -0700

    Agregado el login con model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  { 
+    path: '', redirectTo: 'login', pathMatch: 'full' 
+  },
+  {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'add-product',
     loadChildren: () => import('./add-product/add-product.module').then( m => m.AddProductPageModule)
-  }
-
+  },
 ];
 @NgModule({
   imports: [
